126bccb1fc9a204c104a5393a72c5c16ee6c6367
USER01\kanno <kanno062316@icloud.com>
2024-07-14 07:37:16 +0900

    模擬試験：問題14-4

---
+++ Exercises/test14_4.java
@@ -0,0 +1,28 @@
+class test14_4 {
+    static void methodA() {
+        try {
+            int[] a = new int[3]; // 配列 'a' の長さは3 (インデックスは0, 1, 2)
+            a[3] = 10; // インデックス3は存在しないため、ArrayIndexOutOfBoundsExceptionがスローされる。（有効なインデックスは0, 1, 2）
+            System.out.print("A"); // ここには到達しない
+        } catch(Exception e){
+            System.out.print("B");
+        }
+    }
+    static void methodB() {
+        try {
+            methodA();
+            System.out.print("C");
+        } catch(Exception e) {
+            System.out.print("D");
+        }
+    }
+    public static void main(String[] args) {
+        try {
+            methodB();
+            System.out.print("E");
+        } catch(Exception e) {
+            System.out.print("F");
+        }
+    }
+    // BCEが出力される
+}
