e024bbd5640594d34455054b2fe840c226892733
kanno <kanno062316@icloud.com>
2024-07-14 07:53:24 +0900

    模擬試験：問題14-5

---
+++ Exercises/test14_5.java
@@ -0,0 +1,7 @@
+public class test14_5 {
+    static public void main(String args[]){
+        String str = "abcde";
+        System.out.print(str.charAt(str.length() / 2));
+        // charAt は 0,1,2番目なので「c」が出力される。
+    }
+}
