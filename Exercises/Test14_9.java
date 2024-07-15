8cb714673ab0142959cb8839548e9ab9ed4e7923
kanno <kanno062316@icloud.com>
2024-07-16 08:01:49 +0900

    模擬試験：問題14-9

---
+++ Exercises/Test14_9.java
@@ -0,0 +1,7 @@
+public class Test14_9 {
+    public static void main(String[] args) {
+        int[][] a = {{11, 12}, {21, 22, 23, 24}, {31, 32, 33}};
+        System.out.print(a.length + a[1].length);
+        // lengthは要素数なので「7」が出力される。
+    }
+}
