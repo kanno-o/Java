de264d965cc3a4cb923961657394f67032fbb18d
kanno <kanno062316@icloud.com>
2024-07-18 00:20:03 +0900

    模擬試験：問題14-15

---
+++ Exercises/Test14_15.java
@@ -0,0 +1,10 @@
+class Test14_15 {
+    public static void main(String[] args) {
+        int a = 0xcafe; // 16進数リテラルは、0xまたは0X
+        float b = 12; // 実数
+        String c = "A"; // Stringは文字列かnull
+        byte d = 127; // 8bit -128～127まで
+        boolean e = true; // true か false
+        float f = 0L; // 
+    }
+}
