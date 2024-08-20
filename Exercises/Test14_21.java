4647fb4a49846beccbc847478d7224e000388c91
kanno <kanno062316@icloud.com>
2024-08-20 20:54:36 +0900

    模擬試験：問題14-21

---
+++ Exercises/Test14_21.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class Test14_21 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2017, 1, 2);
+        System.out.print(date);
+    }
+}
